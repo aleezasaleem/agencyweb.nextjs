3bd2501b54e3d51e0fad7070d78a7c116b622425
Aleeza Saleem <150428514+aleezasaleem@users.noreply.github.com>
2024-03-18 01:09:39 +0500

    Update Responsive.tsx

--- src/app/Components/Navigation/Responsive.tsx
+++ src/app/Components/Navigation/Responsive.tsx
@@ -1,13 +1,13 @@
 'use client'
 import React, { useState } from 'react'
-import Nav from './Nav'
+
 
 
 const Responsive = () => {
    
   return (
     <div>
-        <Nav />
+       
         
     </div>
   )
